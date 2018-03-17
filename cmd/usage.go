a0799342e8adf50e902e4d1ddc64dbdaa7e445f6
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2018-03-17 17:24:07 +0100

    format card usage data to console

--- cmd/usage.go
+++ cmd/usage.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"sort"
+	"strconv"
 
 	"github.com/mkarpusiewicz/hearthstone-duster/services"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -14,9 +18,22 @@ var usageCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		cardUsage := services.GetUsageInformation()
 
-		for _, row := range cardUsage.CardsPlayedDetails.Series.Data.All {
-			fmt.Println(row)
+		topPlayed := cardUsage.CardsPlayedDetails.Series.Data.All
+		sort.Slice(topPlayed, func(i, j int) bool {
+			return topPlayed[i].Total > topPlayed[j].Total
+		})
+
+		fmt.Println("\r\nTop 100 played cards:\r\n")
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Card ID", "Times", "Popularity %"})
+		table.SetAutoWrapText(false)
+		table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
+		for _, row := range topPlayed[:100] {
+			table.Append([]string{strconv.FormatInt(row.Id, 10), strconv.FormatInt(row.Total, 10), row.Popularity})
 		}
+
+		table.Render()
 	},
 }
 
