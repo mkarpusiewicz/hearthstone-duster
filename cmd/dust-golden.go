94ad570c8e2950f8024c078673af8120b6c5c9d9
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2018-03-26 18:19:47 +0200

    expand dust golden method, check for rarity and show dust value

--- cmd/dust-golden.go
+++ cmd/dust-golden.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"strconv"
 
 	"github.com/fatih/color"
 	"github.com/mkarpusiewicz/hearthstone-duster/database"
+	"github.com/mkarpusiewicz/hearthstone-duster/services"
 	"github.com/mkarpusiewicz/hearthstone-duster/types"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
@@ -16,24 +19,40 @@ var dustGoldenCmd = &cobra.Command{
 	Short: "Show golden cards which can be dusted",
 	Run: func(cmd *cobra.Command, args []string) {
 		myCards := database.GetMyCards()
+		apiCards := database.GetCardsDatabase()
 
-		golden := getGoldenCards(myCards)
+		golden := getGoldenCards(myCards, apiCards)
+
+		totalDustValue := 0
 
 		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Card", "Golden count", "Normal count", "Golds to dust"})
+		table.SetHeader([]string{"Card", "Rarity", "Golden count", "Normal count", "Golds to dust", "Dust value"})
 		table.SetAutoWrapText(false)
-		table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
+		table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_LEFT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
 		for _, card := range golden {
 			dustString := ""
+			dustValueString := ""
 			if card.ToDust > 0 {
 				dustString = color.GreenString(strconv.Itoa(card.ToDust))
+
+				dustValue := card.ToDust * services.GetDustValue(card.Rarity, true)
+				totalDustValue += dustValue
+				dustValueString = color.GreenString(strconv.Itoa(dustValue))
 			}
 
-			table.Append([]string{color.YellowString(card.Name), color.YellowString(strconv.Itoa(card.GoldenCount)),
-				strconv.Itoa(card.NormalCount), dustString})
+			table.Append([]string{card.Name, services.GetRarityColoredString(card.Rarity, card.Rarity), color.YellowString(strconv.Itoa(card.GoldenCount)),
+				strconv.Itoa(card.NormalCount), dustString, dustValueString})
 		}
-
+		table.SetFooter([]string{" ", " ", " ", " ", "Total", strconv.Itoa(totalDustValue)})
 		table.Render()
+
+		fmt.Printf("\r\n\r\nTo DUST for total %d:\r\n", totalDustValue)
+		for _, card := range golden {
+			if card.ToDust == 0 {
+				continue
+			}
+			fmt.Printf("%d X %s\r\n", card.ToDust, card.Name)
+		}
 	},
 }
 
@@ -41,16 +60,34 @@ func init() {
 	dustCmd.AddCommand(dustGoldenCmd)
 }
 
-func getGoldenCards(myCards []types.MyCard) []goldenCard {
+func getGoldenCards(myCards []types.MyCard, apiCards []types.ApiCard) []goldenCard {
 	var goldenCards []goldenCard
 
 	for _, card := range myCards {
-		if card.IsGold {
-			goldenCards = append(goldenCards, goldenCard{Name: card.Name, GoldenCount: card.Count})
+		if !card.IsGold {
+			continue
 		}
-	}
 
-	// todo: check in database which cards are not basic ones and add dust value from rarity
+		goldenCard := goldenCard{Name: card.Name, GoldenCount: card.Count}
+
+		var cardFound bool
+		for _, apiCard := range apiCards {
+			if apiCard.Name == goldenCard.Name {
+				cardFound = true
+				goldenCard.Rarity = apiCard.Rarity
+			}
+		}
+		if !cardFound {
+			log.Println("WARNING: Card '%s' not found in card database", card.Name)
+			continue
+		}
+
+		if goldenCard.Rarity == "FREE" {
+			continue
+		}
+
+		goldenCards = append(goldenCards, goldenCard)
+	}
 
 	for i, goldenCard := range goldenCards {
 		for _, card := range myCards {
@@ -74,4 +111,5 @@ type goldenCard struct {
 	GoldenCount int
 	NormalCount int
 	ToDust      int
+	Rarity      string
 }
