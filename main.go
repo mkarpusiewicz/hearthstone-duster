ee702fd4cda0eee80059b880a759831070527061
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2018-03-26 17:32:08 +0200

    disable time in logs/errors

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mkarpusiewicz/hearthstone-duster/cmd"
 	"github.com/mkarpusiewicz/hearthstone-duster/database"
 )
 
 func main() {
+	log.SetFlags(0) // disable time in logs
+
 	database.Initialize()
 	defer database.Close()
 
