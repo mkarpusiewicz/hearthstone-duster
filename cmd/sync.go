27139797ba1d64569e50a56304d50103cec8155a
Maciej Karpusiewicz <maciej.karpusiewicz@outlook.com>
2018-03-26 17:53:15 +0200

    change sync order

--- cmd/sync.go
+++ cmd/sync.go
@@ -4,14 +4,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
 	Short: "Sync all: hearthpwn.com, hsreplay.net and hearthstonejson.com",
 	Run: func(cmd *cobra.Command, args []string) {
 		syncDatabaseCmd.Run(cmd, args)
-		syncMineCmd.Run(cmd, args)
 		syncUsageCmd.Run(cmd, args)
+		syncMineCmd.Run(cmd, args)
 	},
 }
 
